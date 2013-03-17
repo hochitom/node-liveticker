796389d9c7544b1185c6e4d42388679d18a3ae86
hochitom <hochitom@icloud.com>
2013-03-17 19:55:18 +0100

    added jquery waypoints plugin, styled fixed matchcard

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,27 @@
+var websocket = function() {
+    var socket = io.connect('http://localhost:3001');
+    socket.on('connect', function(){ console.log("Connected to Server"); });
+    socket.on('newMessage', function(data){ 
+        console.log(data);
 
+        // To-Do: Beautify Date
+        $('#stream').prepend('<li><p>' + data.date + '</p><p>' + data.text + '</p></li>');
+    });    
+}
+
+
+$(document).ready(function(){
+    var cHeight = $('.matchcard').height() + 20;
+
+    websocket();
+
+    $('.matchcard').waypoint(function(direction) {
+        var padding = 0;
+        console.log(direction);
+        if (direction === 'down') {
+            padding = cHeight;
+        }
+
+        $('body').toggleClass('fixedMatchcard').css({'padding-top': padding});
+    }, { offset: 0 });
+});
